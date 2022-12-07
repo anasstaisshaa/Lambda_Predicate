6e14e52ae0167c36381ab88a49500ac7e8f3b999
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2022-12-07 21:29:10 +0200

    refactor Lambda

--- src/edu/AnastasiiaTkachuk/Runner.java
+++ src/edu/AnastasiiaTkachuk/Runner.java
@@ -1,23 +1,12 @@
 package edu.AnastasiiaTkachuk;
 
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class Runner {
     public static void main(String[] args) {
-        Predicate<String> predicateNotNull = new Predicate<String>() {
-            @Override
-            public boolean test(String s) {
-                return s !=null;
-            }
-        };
-
-        Predicate<String> predicateNotEmpty = new Predicate<String>() {
-            @Override
-            public boolean test(String s) {
-                return s.length() > 0;
-            }
-        };
-
+        Predicate<String> predicateNotNull = (Objects::nonNull);
+        Predicate<String> predicateNotEmpty = (str -> str.length() > 0);
         Predicate<String> predicateNotNullEmpty = new Predicate<String>() {
             @Override
             public boolean test(String s) {
